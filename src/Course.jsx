4e50a11f4586667a449a0add073e2f196367434c
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2026-03-12 09:25:47 +0530

    Modified styling for course

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,10 +1,7 @@
 const course1 = "React course"
 function Course(){
-    const styles = {
-        backgroundColor : "yellow"
-    }
     return (
-        <div style={styles}>
+        <div className="card">
             <img src="" alt="" />
             <h3>{course1}</h3>
             <p>This is a html course</p>
