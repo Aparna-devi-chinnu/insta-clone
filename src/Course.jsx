55e8363f594312c180726908b29d3c38ce190795
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2026-03-12 09:37:23 +0530

    Added image with stylings

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,8 +1,9 @@
+import react from './assets/react.png'
 const course1 = "React course"
 function Course(){
     return (
         <div className="card">
-            <img src="" alt="" />
+            <img src={react} alt="" />
             <h3>{course1}</h3>
             <p>This is a html course</p>
         </div>
