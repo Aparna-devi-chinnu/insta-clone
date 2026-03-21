736f8f86762e8d3a43291c592a35a218a1c058fb
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2026-03-21 17:35:18 +0530

    Added rating '

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,10 +1,11 @@
 import Loading from './assets/loading.png'
-function Course({img = Loading , name , price}){
+function Course({img = Loading , name , price, rating=0}){
     return (
         <div className="card">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <p>{price}</p>
+            <span>rating : {rating}</span>
         </div>
     );
 
