a411fc98748bdc88a5ff6a81c81d7398c5b7b22e
Aparna-devi-chinnu <aparnarajavel2@gmail.com>
2026-04-24 15:32:15 +0530

    Added butoon with onClick

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,12 +1,18 @@
 import Loading from './assets/loading.png'
 
 function Course({img = Loading , name , price, rating=0}){
+
+    function buyCourse(percent){
+        console.log(name ,"purchased with ",percent," % discount")
+    }
+
         return (
         <div className="card">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <p>{price}</p>
             <span>rating : {rating}</span>
+            <button onClick={() => buyCourse(20)}>Buy now</button>
         </div>
     );
 }
